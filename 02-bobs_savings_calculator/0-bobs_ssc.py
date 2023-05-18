c635331cb644e176101f797a374207cb85bc20cd
root <gumquat@gmail.com>
2023-05-18 11:29:53 -0700

    :P

---
+++ 02-bobs_savings_calculator/0-bobs_ssc.py
@@ -0,0 +1,16 @@
+#Greet Bob, then calculate his yearly savings using his weekly income information
+greeting = "Hello "
+name = "Bob"
+
+hourly_wage = 20
+hours_per_week = 40
+income_per_week = hourly_wage * hours_per_week
+workweeks_per_year = 48
+income_per_year = workweeks_per_year * income_per_week #calc yearly income
+spend_per_week = 400
+spend_per_year = spend_per_week * 52 #calc yearly spendature
+
+print(greeting + name) #greeting
+print(name + ", your yearly income is: {:d}".format(income_per_year)) #print yearly income
+print(name + ", your yearly spendature is: {:d}".format(spend_per_year)) #print yearly spendature
+print(name + ", your yearly savings comes to a total of {:d}".format(income_per_year - spend_per_year))
