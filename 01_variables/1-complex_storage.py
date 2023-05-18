26c724be0daa86e9c9cc9f2a3cf1fb3fca34d81e
root <gumquat@gmail.com>
2023-05-18 11:06:24 -0700

    :P

--- 01_variables/1-complex_storage.py
+++ 01_variables/1-complex_storage.py
@@ -1,5 +1,7 @@
-price = 1
-quantity = 2
-total_cost = a + b
+#write a program that stores number values into two variables named
+#'price' and 'quantity', then set a variable 'total_cost' to their multiplied value
+#lastly, print the value stored in 'total_cost' 
+price = 3
+quantity = 10
 total_cost = price * quantity
 print(total_cost)
