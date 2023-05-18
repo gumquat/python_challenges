4a2b804c69ac1cf1d4931bd60f77ff9067023055
root <gumquat@gmail.com>
2023-05-18 11:11:39 -0700

    :P

---
+++ 01_variables/2-string_combination.py
@@ -0,0 +1,7 @@
+#write a program that assigns the words 'fire' and 'works' to different variables
+#then assigned a combination of their variables to a variable named 'combination'
+#lastly, print the value stored in 'combination'
+word1 = "fire"
+word2 = "works"
+combination = word1 + word2
+print(combination)
