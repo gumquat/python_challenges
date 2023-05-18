1290db33f73ad19d89fb8c95509adc7256a66bc3
root <gumquat@gmail.com>
2023-05-18 11:02:50 -0700

    :P

---
+++ 01_variables/0-storing_&_printing.py
@@ -0,0 +1,8 @@
+#write a program that stores the value 123 in a variable named my_number
+#then use a print statement to print the value stored inside my_number
+#repeat this for a string
+my_number = 123
+my_word = "program"
+
+print(my_number)
+print(my_word)
---
+++ 01_variables/1-complex_storage.py
@@ -0,0 +1,5 @@
+price = 1
+quantity = 2
+total_cost = a + b
+total_cost = price * quantity
+print(total_cost)
