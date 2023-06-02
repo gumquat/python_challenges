d924077d75cf17f4722f7b0a7313c695d112abf9
root <gumquat@gmail.com>
2023-06-02 12:31:39 -0700

    :P

--- edabit_python_challenges/06-find_the_discount.py
+++ edabit_python_challenges/06-find_the_discount.py
@@ -5,3 +5,6 @@ def find_the_discount(price, discount):
         return final_price
     else:
         return ValueError("One of the inputs is not type int")
+
+print("The input values are: Price :100, Discount: 25. And should return 'Discounted price is 75.0' on the next line")
+print("Discounted price is {}".format(find_the_discount(100, 25)))
